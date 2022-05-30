9f57e9d18f59d34f36b17aaba2b4b389c6bd5cac
Radek2894 <85341631+Radek2894@users.noreply.github.com>
2022-05-30 11:20:02 +0200

    Add project files.

---
+++ Middleware/UserAgent.cs
@@ -0,0 +1,33 @@
+﻿namespace Middleware
+{
+    public class UserAgent
+    {
+        private RequestDelegate _next;
+
+        public UserAgent(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            var r = context.Request.Headers["User-Agent"].ToString();
+
+            if (r.Contains("Edg"))
+            {
+                context.Response.Redirect("https://www.mozilla.org/pl/firefox/new/%22");
+            }
+
+            return _next(context);
+        }
+
+    }
+
+    public static class UserAgentMiddleware
+    {
+        public static IApplicationBuilder UseUrlTransformMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<UserAgent>();
+        }
+    }
+}
